a1fadd3091667beccda6e2977c72db835f250f9b
Idorma <69322475+Idorma@users.noreply.github.com>
2021-11-11 23:37:31 +0100

    Revert "cambios controlador"
    This reverts commit b0773ce38fdf7871166e7a3c4a9787317ba6f964.

--- controller/SignUpController.java
+++ controller/SignUpController.java
@@ -1,24 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import classes.User;
 import exceptions.*;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -28,15 +20,19 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import static logic.UiLogicFactory.getUiImplem;
 
 /**
+ * El controlador de la ventana de SignUp la cual controla las excepciones y las
+ * acciones de los botones y los textos.
  *
- * @author idoia
+ * @author Idoia Ormaetxea y Alain Cosgaya
  */
-public class SignUpController implements Initializable {
+public class SignUpController {
 
+    private static final Logger LOGGER = Logger.getLogger(SignUpController.class.getName());
     @FXML
     private Label lblCaract;
 
@@ -67,153 +63,262 @@ public class SignUpController implements Initializable {
     @FXML
     private Button btnSignUp;
 
+    @FXML
+    private Button btnAtras;
+
+    @FXML
+    private BorderPane paneSignUp;
+
     private final int max = 50;
 
+    private Stage stage;
+
+    /**
+     * El metodo que indica el stage.
+     *
+     * @param stage1
+     */
+    public void setStage(Stage stage1) {
+        stage = stage1;
+    }
+
+    /**
+     * El metodo que instancia la ventana.
+     *
+     * @param root
+     */
+    public void initStage(Parent root) {
+        Stage anotherStage = new Stage();
+        anotherStage.setResizable(false);
+        anotherStage.getIcons().add(new Image(
+                "/photos/descargas-removebg-preview.png")
+        );
+        anotherStage.setTitle("SignUp");
+        anotherStage.setScene(new Scene(root));
+        LOGGER.info("Llamada a metodos y restricciones del controlador");
+        txtEmail.textProperty().addListener(this::emailTextValidation);
+        txtPasswd.textProperty().addListener(this::passwdTextCaractValidation);
+        txtPasswd.textProperty().addListener(this::passwdTextNumValidation);
+        txtPassw2.textProperty().addListener(this::repeatpasswd);
+        reportedFields();
+        anotherStage.show();
+    }
+
+    /**
+     * El metodo que controla que no se introduzcan espacios en los campos de
+     * texto.
+     *
+     * @param event
+     */
     @FXML
     private void eventKey(KeyEvent event) {
         Object evt = event.getSource();
 
-        if (evt.equals(txtUser) || evt.equals(txtPasswd) || evt.equals(txtPassw2) || evt.equals(txtEmail)) {
+        if (evt.equals(txtUser) || evt.equals(txtPasswd) || evt.equals(txtPassw2)
+                || evt.equals(txtEmail)) {
             if (event.getCharacter().equals(" ")) {
                 event.consume();
             }
         }
-        if (txtUser.getText().length() >= max) {
-            event.consume();
+        if (evt.equals(txtName)) {
+            if (txtName.getText().length() >= max) {
+                event.consume();
+            }
         }
-        if (txtPasswd.getText().length() >= max) {
-            event.consume();
+        if (evt.equals(txtUser)) {
+            if (txtUser.getText().length() >= max) {
+                event.consume();
+            }
         }
-        if (txtPassw2.getText().length() >= max) {
-            event.consume();
+        if (evt.equals(txtPasswd)) {
+            if (txtPasswd.getText().length() >= max) {
+                event.consume();
+            }
         }
-        if (txtEmail.getText().length() >= max) {
-            event.consume();
+        if (evt.equals(txtPassw2)) {
+            if (txtPassw2.getText().length() >= max) {
+                event.consume();
+            }
         }
-        if (txtName.getText().length() >= max) {
-            event.consume();
+        if (evt.equals(txtEmail)) {
+            if (txtEmail.getText().length() >= max) {
+                event.consume();
+            }
         }
 
     }
 
+    /**
+     * El metodo que indica las acciones del botón y crea la ventana a la que se
+     * dirige, llevandole los datos.
+     *
+     * @param event
+     */
     @FXML
-    private void buttonEvent(ActionEvent event) throws IOException {
+    private void buttonEvent(ActionEvent event) {
 
         try {
+            LOGGER.info("Inicializacion de la variable user");
             User user = new User();
             user.setUsername(txtUser.getText());
             user.setFullName(txtName.getText());
             user.setEmail(txtEmail.getText());
             user.setPassword(txtPasswd.getText());
+            LOGGER.info("Ejecucion del metodo signUp de la implementacion");
             user = getUiImplem().signUp(user);
-            Parent anotherRoot = FXMLLoader.load(getClass().getResource("/view/Session.fxml"));
-            Stage anotherStage = new Stage();
-            anotherStage.setResizable(false);
-            anotherStage.getIcons().add(new Image("/photos/descargas-removebg-preview.png"));
-            anotherStage.setTitle("Session");
-            anotherStage.setScene(new Scene(anotherRoot));
-            anotherStage.show();
+            LOGGER.info("Registro de usuario exitoso");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Usuario registrado correctamente");
+            alert.show();
+            LOGGER.info("Carga del FXML de Session");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "/view/Session.fxml")
+            );
 
-            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent root = (Parent) loader.load();
+            LOGGER.info("Llamada al controlador del FXML");
+            SessionController controller = ((SessionController) loader.getController());
+            controller.setStage(stage);
+            controller.initData(user);
+            controller.initStage(root);
+
+            paneSignUp.getScene().getWindow().hide();
 
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ConnectException | SignUpException | UpdateException ex) {
+            Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, e);
+        } catch (ConnectException | SignUpException | UpdateException | ServerFullException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR, ex.getMessage());
             alert.show();
+            Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
 
+    /**
+     * El metodo que indica las acciones del botón y crea la ventana a la que se
+     * dirige.
+     *
+     * @param event
+     */
     @FXML
-    private void buttonEventBack(ActionEvent event) throws IOException {
+    private void buttonEventBack(ActionEvent event) {
 
         try {
-            Parent anotherRoot = FXMLLoader.load(getClass().getResource("/view/SignIn.fxml"));
-            Stage anotherStage = new Stage();
-            anotherStage.setResizable(false);
-            anotherStage.getIcons().add(new Image("/photos/descargas-removebg-preview.png"));
-            anotherStage.setTitle("SignIn");
-            anotherStage.setScene(new Scene(anotherRoot));
-            anotherStage.show();
+            LOGGER.info("Carga del FXML de SignIn");
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "/view/SignIn.fxml")
+            );
 
-            ((Node) (event.getSource())).getScene().getWindow().hide();
+            Parent root = (Parent) loader.load();
+            LOGGER.info("Llamada al controlador del FXML");
+            SignInController controller = ((SignInController) loader.getController());
+            controller.setStage(stage);
+            controller.initStage(root);
+
+            paneSignUp.getScene().getWindow().hide();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, e);
         }
 
     }
 
-    private void emailTextValidation() {
-        txtEmail.textProperty().addListener((ov, oldV, newV) -> {
-            if (!txtEmail.getText().equals("")) {
+    /**
+     * El metodo que valida si el formato de correo es correcto.
+     *
+     * @param ov valor observable
+     * @param oldV valor antiguo
+     * @param newV valor nuevo
+     */
+    private void emailTextValidation(ObservableValue ov, String oldV,
+            String newV) {
 
-                try {
-                    String email = txtEmail.getText();
-                    validarEmailPattern(email);
-                    lblEmail.setVisible(false);
-                } catch (EmailPatternException e) {
-                    lblEmail.setVisible(true);
-                }
-
+        if (!txtEmail.getText().equals("")) {
+            try {
+                String email = txtEmail.getText();
+                validarEmailPattern(email);
+                lblEmail.setVisible(false);
+            } catch (EmailPatternException e) {
+                lblEmail.setVisible(true);
             }
-        });
+        } else {
+            lblEmail.setVisible(false);
+        }
+    }
+
+    /**
+     * El metodo que valida si se han introducido menos de 8 caracteres.
+     *
+     * @param ov valor observable
+     * @param oldV valor antiguo
+     * @param newV valor nuevo
+     */
+    private void passwdTextCaractValidation(ObservableValue ov, String oldV,
+            String newV) {
+
+        if (!txtPasswd.getText().equals("")) {
+
+            try {
+                String passwd = txtPasswd.getText();
+                validarMinCaractPasswdPattern(passwd);
+                lblCaract.setVisible(false);
+            } catch (PasswordLengthException e) {
+                lblCaract.setVisible(true);
+            }
+
+        }else{
+            lblCaract.setVisible(false);
+        }
 
     }
 
-    private void passwdTextCaractValidation() {
+    /**
+     * El metodo que valida si se han introducido números.
+     *
+     * @param ov valor observable
+     * @param oldV valor antiguo
+     * @param newV valor nuevo
+     */
+    private void passwdTextNumValidation(ObservableValue ov, String oldV,
+            String newV) {
 
-        txtPasswd.textProperty().addListener((ov, oldV, newV) -> {
-            if (!txtPasswd.getText().equals("")) {
-
-                try {
-                    String passwd = txtPasswd.getText();
-                    validarMinCaractPasswdPattern(passwd);
-                    lblCaract.setVisible(false);
-                } catch (PasswordLengthException e) {
-                    lblCaract.setVisible(true);
-                }
-
+        if (!txtPasswd.getText().equals("")) {
+            try {
+                String passwd = txtPasswd.getText();
+                validarNumPasswdPattern(passwd);
+                lblNum.setVisible(false);
+            } catch (PasswordNumException e) {
+                lblNum.setVisible(true);
             }
-        });
+        }else{
+            lblNum.setVisible(false);
+        }
     }
 
-    private void passwdTextNumValidation() {
+    /**
+     * El metodo que valida si las contraseñas coinciden entre ellas.
+     *
+     * @param ov valor observable
+     * @param oldV valor antiguo
+     * @param newV valor nuevo
+     */
+    private void repeatpasswd(ObservableValue ov, String oldV, String newV) {
 
-        txtPasswd.textProperty().addListener((ov, oldV, newV) -> {
-            if (!txtPasswd.getText().equals("")) {
-
-                try {
-                    String passwd = txtPasswd.getText();
-                    validarNumPasswdPattern(passwd);
-                    lblNum.setVisible(false);
-                } catch (PasswordNumException e) {
-                    lblNum.setVisible(true);
-                }
-
+        if (!txtPassw2.getText().equals("")) {
+            try {
+                String passwd = txtPassw2.getText();
+                validarEqualPasswd();
+                lblPasswd2.setVisible(false);
+            } catch (SamePasswordException e) {
+                lblPasswd2.setVisible(true);
             }
-        });
-
+        }else{
+            lblPasswd2.setVisible(false);
+        }
     }
 
-    private void repeatpasswd() {
-
-        txtPassw2.textProperty().addListener((ov, oldV, newV) -> {
-            if (!txtPassw2.getText().equals("")) {
-
-                try {
-                    String passwd = txtPassw2.getText();
-                    validarEqualPasswd();
-                    lblPasswd2.setVisible(false);
-                } catch (SamePasswordException e) {
-                    lblPasswd2.setVisible(true);
-                }
-
-            }
-        });
-    }
-
+    /**
+     * El metodo que controla si los campos están informados.
+     */
     private void reportedFields() {
         btnSignUp.disableProperty().bind(
                 txtPasswd.textProperty().isEmpty().or(
@@ -236,16 +341,12 @@ public class SignUpController implements Initializable {
         );
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        emailTextValidation();
-        passwdTextCaractValidation();
-        passwdTextNumValidation();
-        repeatpasswd();
-        reportedFields();
-
-    }
-
+    /**
+     * El metodo que valida que el patron del correo es correcto.
+     *
+     * @param email recoge el valor del correo.
+     * @throws EmailPatternException
+     */
     public void validarEmailPattern(String email) throws EmailPatternException {
 
         String regex = "^(.+)@(.+)[.](.+)$";
@@ -259,7 +360,14 @@ public class SignUpController implements Initializable {
 
     }
 
-    public void validarMinCaractPasswdPattern(String passwd) throws PasswordLengthException {
+    /**
+     * El metodo que controla los caracteres mínimos de la contraseña.
+     *
+     * @param passwd recoge el valor de la contraseña.
+     * @throws PasswordLengthException
+     */
+    public void validarMinCaractPasswdPattern(String passwd)
+            throws PasswordLengthException {
         String regex = "^(.+){8,50}$";
 
         Pattern pattern = Pattern.compile(regex);
@@ -272,7 +380,14 @@ public class SignUpController implements Initializable {
 
     }
 
-    public void validarNumPasswdPattern(String passwd) throws PasswordNumException {
+    /**
+     * El metodo que controla si la contraseña contiene números.
+     *
+     * @param passwd recoge el valor de la contraseña.
+     * @throws PasswordNumException
+     */
+    public void validarNumPasswdPattern(String passwd)
+            throws PasswordNumException {
 
         String regex = ".*\\d.*";
 
@@ -284,6 +399,11 @@ public class SignUpController implements Initializable {
         }
     }
 
+    /**
+     * El metodo que compara que las dos contraseñas son iguales.
+     *
+     * @throws SamePasswordException
+     */
     public void validarEqualPasswd() throws SamePasswordException {
         if (!txtPasswd.getText().equals(txtPassw2.getText())) {
             throw new SamePasswordException(lblPasswd2.getText());
